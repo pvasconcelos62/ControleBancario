6eb4d51a801d839c5bbfa414c0c46d5fdeb81592
pvasconcelos62 <155587724+pvasconcelos62@users.noreply.github.com>
2024-05-07 13:35:42 -0300

    Initial Commit

---
+++ ControleBancario/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ControleBancario
+//
+//  Created by PAULO VASCONCELOS JR on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text( " Novo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ControleBancario/ControleBancarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControleBancarioApp.swift
+//  ControleBancario
+//
+//  Created by PAULO VASCONCELOS JR on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ControleBancarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
